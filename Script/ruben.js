48290b023f5f71b6f2c9804170a087cdde4a4af9
RubenDavidAbreu <rubendavid954@gmail.com>
2023-12-19 14:40:34 +0100

    Correction bug cookie

--- Script/ruben.js
+++ Script/ruben.js
@@ -1,15 +1,21 @@
+
 $(document).ready(function () {
     setTimeout(function () {
         $("#cookie-banner").addClass("show");
     }, 200);
+    
 });
 
 function acceptCookies() {
     $("#cookie-banner").removeClass("show");
+    cookie = document.getElementById('cookie-banner');
+    cookie.classList.add("visibility"); 
     // Ajoutez ici votre logique pour accepter les cookies
 }
 
 function rejectCookies() {
     $("#cookie-banner").removeClass("show");
+    cookie = document.getElementById('cookie-banner');
+    cookie.classList.add("visibility"); 
     // Ajoutez ici votre logique pour refuser les cookies
 }
