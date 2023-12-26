4aba018b7f320815865a2d12a38f0a1fa3af7178
Jeremie <parant.jeremie@gmail.com>
2023-12-26 15:06:12 +0100

    teste

--- formulaire.php
+++ formulaire.php
@@ -10,6 +10,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
 </head>
+<h1>TESTE</h1>
 <body>
     <form  action="formulaire.php" method="post">
         <input type="text" name="name" id="" placeholder="Nom"><br>
