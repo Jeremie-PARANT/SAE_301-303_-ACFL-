d2c6116d5a9ae75ffe1cd41e87b1f2d45a18df0c
Jeremie-PARANT <parantjeremie@gmail.com>
2024-01-02 16:28:59 +0100

    Back : page de connexion fini

---
+++ reservation.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+// Appelle la BDD et de la classe adherent
+require_once 'PHP/database.php';
+$database = new App\Database\database();
+require_once 'PHP/adherent.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
