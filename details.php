fe220e54eb1274611e267de2b62f60e66d004f55
RubenDavidAbreu <rubendavid954@gmail.com>
2024-01-09 18:38:41 +0100

    liaison entre pages

--- details.php
+++ details.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+$autorisation = $_SESSION['autorisation'];
 // Appelle la BDD et de la classe adherent
 require_once 'PHP/database.php';
 $database = new App\Database\database();
@@ -46,11 +48,15 @@ $envoyer = '';
         // Nav bar
         echo '<nav class="navbar navbar-expand-lg navbar-light backgroundDarkBlue fixed-top" id="main-navbar">
                 <ul class="navbar-nav navbar-left mb-0" id="main-menu">
-                    <li class="nav-item navbar-brand mr-4"><a class="navLink ml-2" href="#home">Accueil</a></li>
-                </ul>
-                <ul class="navbar-nav mb-0">
-                    <li class="nav-item navbar-brand mr-4"><a class="navLink2" href="formulaire.php">Inscription</a></li>
-                    <li class="nav-item navbar-brand"><a class="navLink2" href="connexion.php">Connexion</a></li>
+                    <li class="nav-item navbar-brand mr-4"><a class="navLink ml-2" href="profil.php">Profil</a></li>
+                    <li class="nav-item navbar-brand mr-4"><a class="navLink ml-2" href="reservation.php">Réserver</a></li>
+                </ul>';
+                if ($autorisation == 1){
+                    echo '<li class="nav-item navbar-brand mr-4"><a class="navLink ml-2" href="backOfficeTable.php">BackOffice</a></li>';
+                    echo '<li class="nav-item navbar-brand mr-4"><a class="navLink ml-2" href="addPilote.php">AddPilote</a></li>';
+                }
+            echo '<ul class="navbar-nav mb-0">
+                    <li class="nav-item navbar-brand mr-4"><a class="navLink2" href="PHP/deconnecter.php">Se déconnecter</a></li>
                 </ul>
             </nav><br><br><br>';
             echo "<br><h2 class='subTitle'>Détails de la réservations</h2>";
